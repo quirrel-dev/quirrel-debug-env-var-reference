4556b498a5cc3a290267a3b28014553f71b4846f
Simon Knott <info@simonknott.de>
2022-01-03 15:33:09 +0100

    wrong error message, apparently

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { QuirrelClient } from "quirrel";
+import { QuirrelClient } from "quirrel/client";
 
 export default function handler(req, res) {
   const client = new QuirrelClient({
